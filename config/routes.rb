5acf79ca3170c3b12fac70bfaf257661b571e052
Neodelf <neodelf@gmail.com>
2014-10-16 23:43:43 +0400

    added csses, route, start makeup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :conditions, only: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
