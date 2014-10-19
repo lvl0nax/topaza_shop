28c99aa3ed887bb1c85b002bd455e6fb5ffc3520
lvl0nax <lvl0nax@gmail.com>
2014-10-19 22:33:53 +0700

    fix main for images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,11 @@
 class Image < ActiveRecord::Base
   mount_uploader :name, ImageUploader
   default_scope { order('id') }
+  scope :main, ->{ where(main: true).exists? ? where(main: true).first : first }
   belongs_to :imageable, polymorphic: true
+  after_update :set_image_as_main, :if => Proc.new { |img| img.main_changed? }
+  private
+  def set_image_as_main
+    self.imageable.images.where.not(id: self.id).update_all(main: false)
+  end
 end
