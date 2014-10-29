d8185ab6a46fb24e132b3535e3a2d5af5ac48da6
Neodelf <neodelf@gmail.com>
2014-10-29 22:27:02 +0300

    add files for uploading image from ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,6 +2,13 @@ CKEDITOR.editorConfig = function( config ) {
     config.language = 'en';
     config.uiColor = '#D4D4D4';
     config.toolbar = 'Basic';
+    config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+    config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+    config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+    config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+    config.filebrowserUploadUrl = "/ckeditor/attachment_files";
     config.toolbar = [
         { name: 'document', items: [ 'Source' ] },
         { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
