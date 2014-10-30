9145e24ec1a874b4a7127944cd943a607a0d2c72
lvl0nax <lvl0nax@gmail.com>
2014-10-31 00:21:04 +0300

    adding uploads for ckeditor

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,11 @@ task :copy_database_config, roles => :app do
   db_config = "#{shared_path}/database.yml"
   run "cp #{db_config} #{release_path}/config/database.yml"
 end
+after "deploy:update_code", :copy_secrets_config
+task :copy_secrets_config, roles => :app do
+  db_config = "#{shared_path}/secrets.yml"
+  run "cp #{db_config} #{release_path}/config/secrets.yml"
+end
 
 # В rails 3 по умолчанию включена функция assets pipelining,
 # которая позволяет значительно уменьшить размер статических
