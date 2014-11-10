f67abf99ab556b2f200ec6c7e433a132fcb1b392
lvl0nax <lvl0nax@gmail.com>
2014-11-11 00:01:10 +0300

    try fix hstore

--- db/migrate/20141110181951_add_price_content_to_order.rb
+++ db/migrate/20141110181951_add_price_content_to_order.rb
@@ -1,5 +1,6 @@
 class AddPriceContentToOrder < ActiveRecord::Migration
   def change
+    execute 'CREATE EXTENSION IF NOT EXISTS hstore;'
     enable_extension :hstore
     add_column :orders, :price_content, :hstore
   end
